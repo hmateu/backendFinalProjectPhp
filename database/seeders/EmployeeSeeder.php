6ec86339a66a76fa3881b26afc4dc7f3e164ba02
hmateu <hmateu.ortola@gmail.com>
2023-07-13 09:35:22 +0200

    feat: Encrypts user and employee seeders' passwords

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -16,61 +16,61 @@ class EmployeeSeeder extends Seeder
         DB::table('employees')->insert([
             [
                 'email' => 'mangelesTP@gmail.com',
-                'password' => 'MAngeles1234!',
+                'password' => bcrypt('MAngeles1234!'),
                 'user' => 2,
                 'attraction' => 1
             ],
             [
                 'email' => 'jenrique@gmail.com',
-                'password' => 'JEnrique1234!',
+                'password' => bcrypt('JEnrique1234!'),
                 'user' => 3,
                 'attraction' => 1
             ],
             [
                 'email' => 'angelaTP@gmail.com',
-                'password' => 'Angela1234!',
+                'password' => bcrypt('Angela1234!'),
                 'user' => 4,
                 'attraction' => 2
             ],
             [
                 'email' => 'david@gmail.com',
-                'password' => 'David1234!',
+                'password' => bcrypt('David1234!'),
                 'user' => 5,
                 'attraction' => 2
             ],
             [
                 'email' => 'borjaTP@gmail.com',
-                'password' => 'Borja1234!',
+                'password' => bcrypt('Borja1234!'),
                 'user' => 6,
                 'attraction' => 3
             ],
             [
                 'email' => 'sarahTP@gmail.com',
-                'password' => 'Sarah1234!',
+                'password' => bcrypt('Sarah1234!'),
                 'user' => 7,
                 'attraction' => 3
             ],
             [
                 'email' => 'mireiaTP@gmail.com',
-                'password' => 'Mireia1234!',
+                'password' => bcrypt('Mireia1234!'),
                 'user' => 8,
                 'attraction' => 4
             ],
             [
                 'email' => 'danielTP@gmail.com',
-                'password' => 'Daniel1234!',
+                'password' => bcrypt('Daniel1234!'),
                 'user' => 9,
                 'attraction' => 4
             ],
             [
                 'email' => 'carlosTP@gmail.com',
-                'password' => 'Andrea1234!',
+                'password' => bcrypt('Andrea1234!'),
                 'user' => 10,
                 'attraction' => 5
             ],
             [
                 'email' => 'andreaTP@gmail.com',
-                'password' => 'Andrea1234!',
+                'password' => bcrypt('Andrea1234!'),
                 'user' => 11,
                 'attraction' => 5
             ]
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
                 'cp' => 42361,
                 'mobile' => 736554817,
                 'email' => 'marta@gmail.com',
-                'password' => 'Marta1234!',
+                'password' => bcrypt('Marta1234!'),
             ],
             [
                 // 'id' => 2,
@@ -34,7 +34,7 @@ class UserSeeder extends Seeder
                 'cp' => 63218,
                 'mobile' => 723615048,
                 'email' => 'mangeles@gmail.com',
-                'password' => 'MAngeles1234!',
+                'password' => bcrypt('MAngeles1234!'),
             ]
             ,
             [
@@ -46,7 +46,7 @@ class UserSeeder extends Seeder
                 'cp' => 74253,
                 'mobile' => 617592301,
                 'email' => 'jenrique@gmail.com',
-                'password' => 'JEnrique1234!',
+                'password' => bcrypt('JEnrique1234!'),
             ],
             [
                 // 'id' => 4,
@@ -57,7 +57,7 @@ class UserSeeder extends Seeder
                 'cp' => 21035,
                 'mobile' => 742536910,
                 'email' => 'angela@gmail.com',
-                'password' => 'Angela1234!',
+                'password' => bcrypt('Angela1234!'),
             ],
             [
                 // 'id' => 5,
@@ -68,7 +68,7 @@ class UserSeeder extends Seeder
                 'cp' => 63021,
                 'mobile' => 625471038,
                 'email' => 'david@gmail.com',
-                'password' => 'David1234!',
+                'password' => bcrypt('David1234!'),
             ],
             [
                 // 'id' => 6,
@@ -79,7 +79,7 @@ class UserSeeder extends Seeder
                 'cp' => 12547,
                 'mobile' => 639587421,
                 'email' => 'borja@gmail.com',
-                'password' => 'Borja1234!',
+                'password' => bcrypt('Borja1234!'),
             ],
             [
                 // 'id' => 7,
@@ -90,7 +90,7 @@ class UserSeeder extends Seeder
                 'cp' => 63251,
                 'mobile' => 695321478,
                 'email' => 'sarah@gmail.com',
-                'password' => 'Sarah1234!',
+                'password' => bcrypt('Sarah1234!'),
             ],
             [
                 // 'id' => 8,
@@ -101,7 +101,7 @@ class UserSeeder extends Seeder
                 'cp' => 41257,
                 'mobile' => 741369582,
                 'email' => 'mireia@gmail.com',
-                'password' => 'Mireia1234!',
+                'password' => bcrypt('Mireia1234!'),
             ],
             [
                 // 'id' => 9,
@@ -112,7 +112,7 @@ class UserSeeder extends Seeder
                 'cp' => 45632,
                 'mobile' => 632554777,
                 'email' => 'daniel@gmail.com',
-                'password' => 'Daniel1234!',
+                'password' => bcrypt('Daniel1234!'),
             ],
             [
                 // 'id' => 10,
@@ -123,7 +123,7 @@ class UserSeeder extends Seeder
                 'cp' => 31025,
                 'mobile' => 712554789,
                 'email' => 'carlos@gmail.com',
-                'password' => 'Carlos1234!',
+                'password' => bcrypt('Carlos1234!'),
             ],
             [
                 // 'id' => 11,
@@ -134,7 +134,7 @@ class UserSeeder extends Seeder
                 'cp' => 43105,
                 'mobile' => 691422057,
                 'email' => 'andrea@gmail.com',
-                'password' => 'Andrea1234!',
+                'password' => bcrypt('Andrea1234!'),
             ],
             [
                 'dni' => '361472995D',
@@ -144,7 +144,7 @@ class UserSeeder extends Seeder
                 'cp' => 17302,
                 'mobile' => 621433025,
                 'email' => 'emma@gmail.com',
-                'password' => 'Emma1234!',
+                'password' => bcrypt('Emma1234!'),
             ],
             [
                 'dni' => '63225147A',
@@ -154,7 +154,7 @@ class UserSeeder extends Seeder
                 'cp' => 63213,
                 'mobile' => 632100214,
                 'email' => 'susi@gmail.com',
-                'password' => 'Susi1234!',
+                'password' => bcrypt('Susi1234!'),
             ],
             [
                 'dni' => '214753666T',
@@ -164,7 +164,7 @@ class UserSeeder extends Seeder
                 'cp' => 14753,
                 'mobile' => 712365548,
                 'email' => 'jose@gmail.com',
-                'password' => 'Jose1234!',
+                'password' => bcrypt('Jose1234!'),
             ],
             [
                 'dni' => '11536487Q',
@@ -174,7 +174,7 @@ class UserSeeder extends Seeder
                 'cp' => 32154,
                 'mobile' => 712300564,
                 'email' => 'carmen@gmail.com',
-                'password' => 'Carmen1234!',
+                'password' => bcrypt('Carmen1234!'),
             ],
             [
                 'dni' => '38441523H',
@@ -184,7 +184,7 @@ class UserSeeder extends Seeder
                 'cp' => 34872,
                 'mobile' => 663214570,
                 'email' => 'maria@gmail.com',
-                'password' => 'Maria1234!',
+                'password' => bcrypt('Maria1234!'),
             ],
             [
                 'dni' => '951032655P',
@@ -194,7 +194,7 @@ class UserSeeder extends Seeder
                 'cp' => 63214,
                 'mobile' => 726930688,
                 'email' => 'raul@gmail.com',
-                'password' => 'Raul1234!',
+                'password' => bcrypt('Raul1234!'),
             ],
             [
                 'dni' => '24551785S',
@@ -204,7 +204,7 @@ class UserSeeder extends Seeder
                 'cp' => 12477,
                 'mobile' => 722154365,
                 'email' => 'toni@gmail.com',
-                'password' => 'Toni1234!',
+                'password' => bcrypt('Toni1234!'),
             ],
             [
                 'dni' => '631544745K',
@@ -214,7 +214,7 @@ class UserSeeder extends Seeder
                 'cp' => 42530,
                 'mobile' => 623301547,
                 'email' => 'marta1@gmail.com',
-                'password' => 'Marta1234!',
+                'password' => bcrypt('Marta1234!'),
             ],
             [
                 'dni' => '632114587E',
@@ -224,7 +224,7 @@ class UserSeeder extends Seeder
                 'cp' => 74120,
                 'mobile' => 709631548,
                 'email' => 'david1@gmail.com',
-                'password' => 'David1234!',
+                'password' => bcrypt('David1234!'),
             ],
             [
                 'dni' => '74521102U',
@@ -234,7 +234,7 @@ class UserSeeder extends Seeder
                 'cp' => 70215,
                 'mobile' => 702366541,
                 'email' => 'andrea1@gmail.com',
-                'password' => 'Andrea1234!',
+                'password' => bcrypt('Andrea1234!'),
             ],
             [
                 'dni' => '95633214N',
@@ -244,7 +244,7 @@ class UserSeeder extends Seeder
                 'cp' => 63221,
                 'mobile' => 795632002,
                 'email' => 'aitana@gmail.com',
-                'password' => 'Aitana1234!',
+                'password' => bcrypt('Aitana1234!'),
             ],
             [
                 'dni' => '32145541J',
@@ -254,7 +254,7 @@ class UserSeeder extends Seeder
                 'cp' => 95233,
                 'mobile' => 721145032,
                 'email' => 'rafa@gmail.com',
-                'password' => 'Rafa1234!',
+                'password' => bcrypt('Rafa1234!'),
             ]
         ]);
     }
